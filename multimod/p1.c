5daf324140ccc9962cc7eef95f730e3fe15d3a74
tracer-nju <tracer@nju.edu.cn>
2019-11-23 22:16:38 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 86f1ca8507c74c82a0e86088d9de46c9            Boot ID: e3c1cfd5137a42fe97624b01701d8547   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  22:16:38 up 13:23,  1 user,  load average: 1.27, 1.31, 1.19

--- multimod/p1.c
+++ multimod/p1.c
@@ -1,6 +1,11 @@
 #include "multimod.h"
+#define base 2
 
 int64_t multimod_p1(int64_t a, int64_t b, int64_t m) {
   // TODO: implement
+  char num1[64],num2[64],num3[64];
+  for(int i = 0;i < 64;i++){
+    
+  }  
   return -1;
 }
