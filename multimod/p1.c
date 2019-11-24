5f27e53e7cc53d9c12900e650efe9e7ea9736c37
tracer-nju <tracer@nju.edu.cn>
2019-11-24 23:02:18 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 86f1ca8507c74c82a0e86088d9de46c9            Boot ID: f2a0a05e11f3494d9f65ca48b06e6b8f   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  23:02:18 up  3:16,  1 user,  load average: 0.85, 0.67, 0.62

--- multimod/p1.c
+++ multimod/p1.c
@@ -1,37 +1,48 @@
 #include "multimod.h"
 #include <stdio.h>
 #include <string.h>
-#define bit 30
+#define bit 20
 //2的64次方为18446744073709551616，共20位
 int64_t multimod_p1(int64_t a, int64_t b, int64_t m) {
   // TODO: implement
-  
-  char temp1[bit],temp2[bit],temp3[bit];
-  for(int i=0; a != 0&&i<bit;i++){
-    temp1[i] = a%10+'0';
+  int num1[bit],num2[bit],num3[bit];
+  int l1=0,l2=0,l3=0;
+  while(a){
+    num1[l1]=a%10;
     a/=10;
+    l1++;
   }
-  for(int i=0; b != 0&&i<bit;i++){
-    temp2[i] = b%10+'0';
+  while(b){
+    num2[l2]=b%10;
     b/=10;
+    l2++;
   }
-  for(int i=0; m != 0&&i<bit;i++){
-    temp3[i] = m%10+'0';
+  while(m){
+    num3[l3]=m%10;
     m/=10;
+    l3++;
   }
-  /*int l1 = strlen(temp1),l2 = strlen(temp2),l3 = strlen(temp3);
-  int num1[l1],num2[l2],num3[l3];
-  for(int i = 0,j=l1-1;i<l1;i++,j--) num1[i] = temp1[j]-'0';
-  for(int i = 0,j=l2-1;i<l2;i++,j--) num2[i] = temp2[j]-'0';
-  for(int i = 0,j=l3-1;i<l3;i++,j--) num3[i] = temp3[j]-'0';*/
-
-  printf("%s\n",temp1);
-  printf("%s\n",temp2);
-  for (int i = 0;i<strlen(temp3);i++)
-    printf("%c",temp3[i]);
-  printf("\n");
-  printf("%ld",strlen(temp3));
+  printf("use it once %d\n",num3[0]);
+  int t[bit*2];
+  for (int i=0;i<bit*2;i++) t[i]=0;
+  for(int i=0;i<l1;i++)
+    for(int j =0; j<l2;j++)
+      t[i+j]+=num1[i]*num2[j];
+  
+  for(int i = 0;i<l1+l2;i++){
+    if(t[i] >=10){
+      t[i+1]+=t[i]/10;
+      t[i]%10;
+    }
+  }
+  int64_t result=0;
+  int64_t base = 1;
+  for(int i = 0;i<l1+l2;i++){
+    result+=t[i]*base;
+    base*10;
+  }
   
   
-  return -1;
+  
+  return result;
 }
