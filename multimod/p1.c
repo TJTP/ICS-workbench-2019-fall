3daf0a0e97068c9e0e12b92d5e71cd7f6af08dc9
tracer-nju <tracer@nju.edu.cn>
2019-11-24 21:51:33 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 86f1ca8507c74c82a0e86088d9de46c9            Boot ID: f2a0a05e11f3494d9f65ca48b06e6b8f   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  21:51:33 up  2:05,  1 user,  load average: 0.11, 0.33, 0.44

--- multimod/p1.c
+++ multimod/p1.c
@@ -3,8 +3,8 @@
 //2的64次方为18446744073709551616，共20位
 int64_t multimod_p1(int64_t a, int64_t b, int64_t m) {
   // TODO: implement
-  char num1[20],num2[20],num3[20];
-  
+  //char num1[20],num2[20],num3[20];
+
   
   return -1;
 }
