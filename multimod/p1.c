c928f4eaaf55c55f8c6872044fcc04d469cdeec1
tracer-nju <tracer@nju.edu.cn>
2019-11-24 22:15:08 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 86f1ca8507c74c82a0e86088d9de46c9            Boot ID: f2a0a05e11f3494d9f65ca48b06e6b8f   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  22:15:08 up  2:29,  1 user,  load average: 0.22, 0.46, 0.50

--- multimod/p1.c
+++ multimod/p1.c
@@ -1,24 +1,27 @@
 #include "multimod.h"
 #include <stdio.h>
+#include<string.h>
 #define bit 30
 //2的64次方为18446744073709551616，共20位
 int64_t multimod_p1(int64_t a, int64_t b, int64_t m) {
   // TODO: implement
   
-  char num1[bit];//,num2[bit],num3[bit];
+  int num1[bit];//num2[bit],num3[bit];
   for(int i=0; a != 0&&i<bit;i++){
-    num1[i] = a%10+'0';
-    a=a/10;
-    printf("%c",num1[i]);
+    num1[i] = a%10;
+    a/=10;
   }
-  /*printf("\n");
-  for(int i=0; b != 0||i<bit;i++){
-    num2[i] = b%10+'0';
+  /*for(int i=0; b != 0||i<bit;i++){
+    num2[i] = b%10;
+    b/=10;
   }
   for(int i=0; m != 0||i<bit;i++){
-    num3[i] = m%10+'0';
+    num3[i] = m%10;
+    m/=10;
   }*/
-
+  for(int i = strlen(num1)-1;i>=0;i--)
+    printf("%d",num1[i]);
+  printf("\n");
   
   return -1;
 }
