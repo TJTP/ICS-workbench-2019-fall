708bf32b44eddcbb2c4a76b841a2a4aa4e66d9a0
tracer-nju <tracer@nju.edu.cn>
2019-11-24 22:05:31 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 86f1ca8507c74c82a0e86088d9de46c9            Boot ID: f2a0a05e11f3494d9f65ca48b06e6b8f   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  22:05:32 up  2:19,  1 user,  load average: 0.64, 0.50, 0.49

--- multimod/p1.c
+++ multimod/p1.c
@@ -5,18 +5,18 @@
 int64_t multimod_p1(int64_t a, int64_t b, int64_t m) {
   // TODO: implement
   
-  char num1[bit],num2[bit],num3[bit];
+  char num1[bit];//,num2[bit],num3[bit];
   for(int i=0; a != 0||i<bit;i++){
     num1[i] = a%10+'0';
-    printf("%s",num1[i]);
+    printf("%c",num1[i]);
   }
-  printf("\n");
+  /*printf("\n");
   for(int i=0; b != 0||i<bit;i++){
     num2[i] = b%10+'0';
   }
   for(int i=0; m != 0||i<bit;i++){
     num3[i] = m%10+'0';
-  }
+  }*/
 
   
   return -1;
