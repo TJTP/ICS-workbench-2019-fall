ab643ed37f4b304161250a650ef3c7a3dac1a091
tracer-nju <tracer@nju.edu.cn>
2019-11-25 22:04:23 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 86f1ca8507c74c82a0e86088d9de46c9            Boot ID: 6867ef17743945838734cf5aabc3e6bc   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  22:04:23 up  6:59,  1 user,  load average: 0.41, 0.28, 0.29

--- multimod/p2.c
+++ multimod/p2.c
@@ -2,15 +2,6 @@
 #include <time.h>
 #include <stdio.h>
 
-#define bit 20
-//2的64次方为18446744073709551616，共20位
-
-typedef struct {
-  int len;
-  int s[bit*2];
-} NUM;
-extern static NUM subtract(NUM t,NUM m);
-
 int64_t multimod_p2(int64_t a, int64_t b, int64_t m) {
   // TODO: implement
   return -2;
