2431fecc4fe3520a40dfec74b30a51c3817e7904
tracer-nju <tracer@nju.edu.cn>
2019-11-25 22:15:04 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 86f1ca8507c74c82a0e86088d9de46c9            Boot ID: 6867ef17743945838734cf5aabc3e6bc   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  22:15:04 up  7:10,  1 user,  load average: 0.39, 0.19, 0.21

--- multimod/p2.c
+++ multimod/p2.c
@@ -8,7 +8,7 @@ int64_t multimod_p2(int64_t a, int64_t b, int64_t m) {
   // TODO: implement
   int bnum1[bit];
   for (int i = 0;i < bit;i++){
-    if((a>>i)&1) bnum1[i]=1;
+    if((a>>i)&0b1) bnum1[i]=1;
   }
   for(int i = 62;i>=0;i--) printf("%d",bnum1[i]);
   printf("\n");
