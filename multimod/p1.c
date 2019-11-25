6091d58fd059aac1239a007ef85fb848813fe544
tracer-nju <tracer@nju.edu.cn>
2019-11-25 18:58:51 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 86f1ca8507c74c82a0e86088d9de46c9            Boot ID: 6867ef17743945838734cf5aabc3e6bc   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  18:58:52 up  3:53,  1 user,  load average: 0.95, 0.78, 0.70

--- multimod/p1.c
+++ multimod/p1.c
@@ -56,16 +56,41 @@ int64_t multimod_p1(int64_t a, int64_t b, int64_t m) {
     }
     return result;
   }
+  NUM r = subtract(tmp,num3);
+  int64_t result = 0;
+  int64_t base = 1;
+  for(int i = 0;i<r.len;i++){
+    result+=r.s[i]*base;
+    base*=10;
+  }
+  return result;
+}
 
-
-
-
-
-
+NUM subtract(NUM t,NUM m){
+  NUM r;
+  r=t;
+  while(1){
+    r=t;
+    for (int i=0;i<m.len;i++){//逐位作差
+      t.s[i] = t.s[i]-m.s[i];
+      if(t.s[i]<0){
+        int j = i+1;
+        for (;j<t.len;j++){//借到位之后
+          if(t.s[j]>0){
+            t.s[j]-=1;
+            for(int k=j-1;k>i;k--) t.s[k]=9;
+            t.s[i]+=10;
+            break;
+          }
+        }
+        if(j==t.len) return r;//此时被减数已经小于减数，即得到余数
+        if(j == t.len-1 && t.s[j]==0) t.len-=1;//最高位变为0后，长度缩小为1；
+      }
+    }
+  }
+  return r;
   
   
-  
-  return -1;
 }
 
 
