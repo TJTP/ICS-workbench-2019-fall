18dfcedfa8ccd9efca0c79cde94f858083b1529d
tracer-nju <tracer@nju.edu.cn>
2019-11-25 20:15:36 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 86f1ca8507c74c82a0e86088d9de46c9            Boot ID: 6867ef17743945838734cf5aabc3e6bc   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  20:15:36 up  5:10,  1 user,  load average: 0.48, 0.54, 0.62

--- multimod/p1.c
+++ multimod/p1.c
@@ -46,7 +46,7 @@ int64_t multimod_p1(int64_t a, int64_t b, int64_t m) {
     }
     if (i == num1.len+num2.len-1 && tmp.s[i] != 0) tmp.len+=1;
   }
-  return tmp.len;
+  //return tmp.len;
   
   if(tmp.len < num3.len){//被除数长度小于除数长度时
     int64_t result = 0;
