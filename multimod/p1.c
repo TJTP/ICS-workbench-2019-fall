c810a10d9f7f9350d19caafc98a86c282a4e7df2
tracer-nju <tracer@nju.edu.cn>
2019-11-26 20:12:18 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 86f1ca8507c74c82a0e86088d9de46c9            Boot ID: 2b1b4edf510343389de9f9df6baacdac   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  20:12:18 up 11:04,  1 user,  load average: 1.29, 1.30, 1.23

--- multimod/p1.c
+++ multimod/p1.c
@@ -28,9 +28,10 @@ int64_t multimod_p1(int64_t a, int64_t b, int64_t m) {
     printf("can't open file\n");
     return false;
   }
-  
-  while(!feof(fp)){
-    fscanf(fp,"%ld %ld %ld",&a,&b,&m);
+  bool flag = true;
+  while(flag){
+    int f = fscanf(fp,"%ld %ld %ld",&a,&b,&m);
+    if(!f) flag = false;
     int64_t cal = calculator(a,b,m);
     printf("a:%20ld b:%20ld m:%20ld ans:%20ld\n",a,b,m,cal);
   }
