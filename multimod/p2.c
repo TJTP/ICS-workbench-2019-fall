04ea80ff18a36a3b07aa2fe1fefe682dd6706315
tracer-nju <tracer@nju.edu.cn>
2019-12-23 22:54:18 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 86f1ca8507c74c82a0e86088d9de46c9            Boot ID: 20f8b6ac73394164908590f502891e0b   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  22:54:18 up 1 day, 13:42,  1 user,  load average: 0.54, 0.46, 0.42

--- multimod/p2.c
+++ multimod/p2.c
@@ -41,29 +41,21 @@ int64_t multimod_p2(int64_t a, int64_t b, int64_t m) {
 
 
 static int64_t calculator(int64_t a, int64_t b, int64_t m){
-  int bnum1[bit];
-  for (int i = 0;i < bit;i++){
-    if((a>>i)&0b1) bnum1[i]=1;
-    else bnum1[i]=0;
-  }
-  int64_t r=0;
-  if(b>(m>>1)&&b<m){
-    for(int i = 0;i<bit;i++){
-      int tmp = b;
-      for(int j = 0;j<i;j++){
-        tmp=(tmp-m+tmp)%m;
-      }
-      r=r+bnum1[i]*tmp;
+  uint64_t res = 0;
+  if (m == 1) return 0;
+  if (m == 2) return (a & 1) && (b & 1);
+  while (a > 0){
+    if (a & 0x1)
+      res = (res % m + b % m) % m;
+    if (b > m)
+      b = (b % m) << 1;
+    else{
+      if (b > (m >> 1))
+        b = (b - m + b) % m;
+      else 
+        b <<= 1;
     }
+    a >>= 1;
   }
-  else{
-    for(int i = 0;i<bit;i++){
-      int tmp = b;
-      r+=(bnum1[i]*(tmp<<i))%m;
-    }
-    r%=m;
-  }
-
- 
-  return r;
+  return (int64_t)res % m;
 }
