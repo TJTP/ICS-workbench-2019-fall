9450a7700091fd4306cd438d2879ab1cd8a5f7d6
tracer-nju <tracer@nju.edu.cn>
2019-12-24 21:34:24 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 86f1ca8507c74c82a0e86088d9de46c9            Boot ID: 20f8b6ac73394164908590f502891e0b   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  21:34:25 up 2 days, 12:23,  1 user,  load average: 0.36, 0.38, 0.36

--- multimod/p2.c
+++ multimod/p2.c
@@ -22,13 +22,19 @@ int64_t multimod_p2(int64_t a, int64_t b, int64_t m) {
     printf("can't open file\n");
     return false;
   }
-  
+  int cnt_right = 0;
+  int cnt_total = 0;
   while(!feof(fp)){
     fscanf(fp,"%ld %ld %ld %ld",&a, &b, &m, &res);
     uint64_t ans = calculator(a,b,m);
-    printf("a:%20ld b:%20ld m:%20ld ref_ans:%20ld my_ans:%20ld cor:%s\n", a, b, m, res, ans, res - ans ? "WRONG" : "RIGHT");
+    bool cor = res - ans == 0 ? true : false;
+    if (cor)
+      cnt_right += 1;
+    printf("a:%20ld b:%20ld m:%20ld ref_ans:%20ld my_ans:%20ld cor:%s\n", a, b, m, res, ans, cor ? "RIGHT" : "WRONG");
+    cnt_total += 1;
   }
   fclose(fp);
+  printf("There are %d samples in total, and # of correct samples is %d\n", cnt_total, cnt_right);
   ret = calculator(a1,b1,m1);
 #endif
 #ifndef COUNTING
@@ -36,7 +42,7 @@ int64_t multimod_p2(int64_t a, int64_t b, int64_t m) {
 #endif
   finish=clock();
   double duration = (double)(finish-start)/CLOCKS_PER_SEC;
-  printf("%f seconds on p3.c and the answer is ",duration);
+  printf("%f seconds on p2.c and the answer is ",duration);
   return ret;
   
 }
