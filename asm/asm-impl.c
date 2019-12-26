5826483f5d4b7efeff350e3d6bb05d332b52fee8
tracer-nju <tracer@nju.edu.cn>
2019-12-26 15:48:06 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 86f1ca8507c74c82a0e86088d9de46c9            Boot ID: bb8c97ae50554b829b154e6662595f46   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  15:48:06 up  3:11,  1 user,  load average: 0.90, 0.95, 0.99

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -69,7 +69,7 @@ void *asm_memcpy(void *dest, const void *src, size_t n) {
 
 int asm_setjmp(asm_jmp_buf env) {
   // TODO: implement
-  asm("mov %0, %%rdi\n"
+  asm(/*"mov %0, %%rdi\n"
       "mov %%rbx, (%%rdi)\n"
       "mov %%rbp, %%rax\n"
       "mov %%rax, 0x8(%%rdi)\n"
@@ -83,7 +83,22 @@ int asm_setjmp(asm_jmp_buf env) {
       "mov %%rax, 0x38(%%rdi)\n" 
       :
       :"m"(env) //占位符0
-      :"%rax", "memory"
+      :"%rax", "memory"*/
+      "mov %[env], %%rdx\n"
+      "mov %%rbx, (%%rdx)\n" // * 保存rbx
+      "mov (%%rsp), %%rax\n"
+      "mov %%rax, 0x8(%%rdx)\n" // * rsp存放rbp的旧址
+      "mov %%r12, 0x10(%%rdx)\n"
+      "mov %%r13, 0x18(%%rdx)\n"
+      "mov %%r14, 0x20(%%rdx)\n"
+      "mov %%r15, 0x28(%%rdx)\n"
+      "lea 0x10(%%rsp), %%rax\n"
+      "mov %%rax, 0x30(%%rdx)\n" // * rsp+10的地址是rsp的旧值
+      "mov 0x8(%%rsp), %%rax\n"
+      "mov %%rax, 0x38(%%rdx)\n"  // * rsp+8存放pc
+      : 
+      : [env] "m"(env)
+      : "%rax", "cc", "memory"
   );
   return 0;
 }
