6f7b80f8714e8b58a1c3a1660b168a8da8794b20
tracer-nju <tracer@nju.edu.cn>
2019-12-26 15:25:39 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 86f1ca8507c74c82a0e86088d9de46c9            Boot ID: bb8c97ae50554b829b154e6662595f46   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  15:25:39 up  2:49,  1 user,  load average: 0.90, 0.98, 0.97

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -90,12 +90,11 @@ int asm_setjmp(asm_jmp_buf env) {
 
 void asm_longjmp(asm_jmp_buf env, int val) {
   // TODO: implement
-  /*asm("mov %0, %%rdi\n"
+  asm("mov %0, %%rdi\n"
       "mov (%%rdi), %%rbx\n"
       "mov 0x30(%%rdi), %%r8\n"
       "mov 0x8(%%rdi), %%r9\n"
       "mov 0x38(%%rdi), %%rdx\n"//pc的值先存入rdx，在后面通过jmp指令实现pc寄存器加载
-      "mov (%%rdi), %%rbx\n"
       "mov 0x10(%%rdi), %%r12\n"
       "mov 0x18(%%rdi), %%r13\n"
       "mov 0x20(%%rdi), %%r14\n"
@@ -108,5 +107,5 @@ void asm_longjmp(asm_jmp_buf env, int val) {
       :"m"(env), "m"(val) //占位符0， 1
       :"memory"
 
-  );*/
+  );
 }
