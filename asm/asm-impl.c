7ef38ee84b1af04545f09223f00c9ae4ea0301f1
tracer-nju <tracer@nju.edu.cn>
2019-12-26 16:46:02 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 86f1ca8507c74c82a0e86088d9de46c9            Boot ID: bb8c97ae50554b829b154e6662595f46   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  16:46:02 up  4:09,  1 user,  load average: 0.24, 0.19, 0.22

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -83,7 +83,7 @@ int asm_setjmp(asm_jmp_buf env) {
       "mov %%rax, 0x38(%%rdi)\n" 
       :
       :"m"(env) //占位符0
-      :"%rax", "memory",  "%rdx"
+      :"%rax", "memory",  "%rdx", "rdi"
   );
   return 0;
 }
