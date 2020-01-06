7ff0b3eb4fc2e6bb520a502b20173caba1547f3f
tracer-nju <tracer@nju.edu.cn>
2020-01-06 10:51:27 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 86f1ca8507c74c82a0e86088d9de46c9            Boot ID: fb5d1bf19c074061ac56806aaae6ac98   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  10:51:27 up 4 days, 12:01,  2 users,  load average: 2.29, 2.24, 2.20

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -1,24 +1,111 @@
 #include "common.h"
 #include <inttypes.h>
+#include <stdlib.h>
 
 void mem_read(uintptr_t block_num, uint8_t *buf);
 void mem_write(uintptr_t block_num, const uint8_t *buf);
+static uint32_t load_cache(uintptr_t);
+static uint32_t substitude_cache(uintptr_t);
 
 static uint64_t cycle_cnt = 0;
+static cache_line *cache;//cache
 
 void cycle_increase(int n) { cycle_cnt += n; }
 
 // TODO: implement the following functions
 
+// 从cache中读出`addr`地址处的4字节数据
+// 若缺失, 需要先从内存中读入数据
 uint32_t cache_read(uintptr_t addr) {
-  return 0;
+  uint32_t grp_idx = ADDR_INDEX(addr); //所给地址中的组号
+  cache_line* base_addr = &cache[exp2(asso_width) * grp_idx]; //对应组的第一行
+
+  //查看对应组中是否命中
+  for (int i = 0; i < exp2(asso_width); i++){
+    if ((base_addr[i].tag == ADDR_TAG(addr)) && base_addr[i].valid_bit == true)
+      return (uint32_t*)(base_addr[i].data + ADDR_IN_BLOCK(addr));
+  }
+  //如果没有命中，则需要加载cache
+  uint32_t idx_in_grp = load_cache(addr);
+  return (uint32_t*)(base_addr[idx_in_grp].data + ADDR_IN_BLOCK(addr));
+
 }
 
+// 往cache中`addr`地址所属的块写入数据`data`, 写掩码为`wmask`
+// 例如当`wmask`为`0xff`时, 只写入低8比特
+// 若缺失, 需要从先内存中读入数据
 void cache_write(uintptr_t addr, uint32_t data, uint32_t wmask) {
+  uint32_t grp_idx = ADDR_INDEX(addr); //所给地址中的组号
+  cache_line* base_addr = &cache[exp2(asso_width) * grp_idx]; //对应组的第一行
+
+  //查看对应组中是否命中
+  for (int i = 0; i < exp2(asso_width); i++){
+    if ((base_addr[i].tag == ADDR_TAG(addr)) && base_addr[i].valid_bit == true){
+      uint32_t *addr_modified = base_addr[i].data + ADDR_IN_BLOCK(addr);
+      *addr_modified = (*addr_modified & ~wmask) | (data & wmask);
+      base_addr[i].dirty_bit = true;
+      return;
+    }
+  }
+
+  //如果没有命中，则需要加载cache
+  uint32_t idx_in_grp =  load_cache(addr);
+  uint32_t *addr_modified = base_addr[idx_in_grp].data + ADDR_IN_BLOCK(addr);
+  *addr_modified = (*addr_modified & ~wmask) | (data & wmask);
+  base_addr[idx_in_grp].dirty_bit = true;
 }
 
+// 初始化一个数据大小为`2^total_size_width`B, 关联度为`2^associativity_width`的cache
+// 例如`init_cache(14, 2)`将初始化一个16KB, 4路组相联的cache
+// 将所有valid bit置为无效即可
 void init_cache(int total_size_width, int associativity_width) {
+  assert(total_size_width >= associativity_width);
+
+  row_num = exp2(total_size_width - BLOCK_WIDTH);
+  cache = (cache_line*)malloc(sizeof(cache_line) * row_num);
+
+  index_width = (total_size_width - BLOCK_WIDTH) - associativity_width;
+  tag_width = MEM_WIDTH - BLOCK_WIDTH - index_width;
+  asso_width = associativity_width;
+
+  for (int i = 0; i < row_num; i++){
+    cache[i].valid_bit = false;
+    cache[i].dirty_bit = false;
+  }
 }
 
 void display_statistic(void) {
+  
+}
+
+uint32_t load_cache(uintptr_t addr){//返回所在组中的行号
+  uint32_t grp_idx = ADDR_INDEX(addr); //所给地址中的组号
+  cache_line* base_addr = &cache[exp2(asso_width) * grp_idx]; //对应组的第一行
+
+  //从内存中取相应块的内容存入对应组的某一行
+  for (uint32_t i = 0; i <exp2(asso_width); i++ ){
+    if (base_addr[i].valid_bit == false){//如果有未被使用的行
+      mem_read(BLOCK_INDEX(addr), base_addr[i].data);
+      base_addr[i].valid_bit = true;
+      base_addr[i].tag = ADDR_TAG(addr);
+      return i;
+    }
+  }
+
+  return substitude_cache(base_addr);
+}
+
+uint32_t substitude_cache(uintptr_t addr){
+  uint32_t grp_idx = ADDR_INDEX(addr); //所给地址中的组号
+  cache_line* base_addr = &cache[exp2(asso_width) * grp_idx]; //对应组的第一行
+  uint32_t idx_in_grp = rand() % exp2(asso_width);
+  
+  if (base_addr[idx_in_grp].dirty_bit == true)//如果是修改过的cache，需要先写回内存
+    mem_write(BLOCK_INDEX(addr), base_addr[idx_in_grp].data);
+  
+  mem_read(BLOCK_INDEX(addr), base_addr[idx_in_grp].data);
+  base_addr[idx_in_grp].valid_bit = true;
+  base_addr[idx_in_grp].tag = ADDR_TAG(addr);
+
+  return idx_in_grp;
 }
--- cachesim/common.h
+++ cachesim/common.h
@@ -9,7 +9,9 @@
 #define exp2(x) (1 << (x))
 #define mask_with_len(x) (exp2(x) - 1)
 
-#define MEM_SIZE (1 << 25) // 1MB
+#define MEM_WIDTH 25
+//#define MEM_SIZE (1 << 25) // 1MB  ?????
+#define MEM_SIZE exp2(MEM_WIDTH) //32MB ?
 #define BLOCK_WIDTH  6  // 64B
 #define BLOCK_SIZE exp2(BLOCK_WIDTH)
 
@@ -18,5 +20,27 @@ typedef uint8_t bool;
 #define false 0
 
 void cycle_increase(int n);
+
+//cache结构相关
+typedef struct {
+    uint32_t valid_bit; //有效位
+    uint32_t dirty_bit; //脏(修改)位
+    uint32_t tag;
+    uint8_t data[BLOCK_SIZE]; //8 * 64
+} cache_line;
+
+uint32_t row_num;  //cache的行数
+uint32_t index_width; //cache的组号的幂(宽度)
+uint32_t tag_width; //cache的标记的宽度
+uint32_t asso_width; //每组的宽度
+
+#define ADDR_INDEX(addr) ((addr >> BLOCK_WIDTH) & mask_with_len(index_width))
+#define ADDR_TAG(addr) ((addr >> (MEM_WIDTH - tag_width)) & mask_with_len(tag_width))
+#define ADDR_IN_BLOCK(addr) (addr & mask_with_len(BLOCK_WIDTH))
+#define BLOCK_INDEX(addr) ((addr >> BLOCK_WIDTH) & mask_with_len(MEM_WIDTH - BLOCK_WIDTH)) 
+
+
+
+
 
 #endif
