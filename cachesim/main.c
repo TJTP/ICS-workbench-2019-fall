b18f2772e528c776df258242b07584138a8c29a2
tracer-nju <tracer@nju.edu.cn>
2020-01-08 20:10:45 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 86f1ca8507c74c82a0e86088d9de46c9            Boot ID: dd93c3380e2849ac9c187dad6ce4aef5   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  20:10:45 up  9:23,  1 user,  load average: 1.38, 0.60, 0.38

--- cachesim/main.c
+++ cachesim/main.c
@@ -3,8 +3,8 @@
 #include <unistd.h>
 
 
-#define TOT_W 12
-#define GRP_W 2
+#define TOT_W 13
+#define GRP_W 1
 
 uint32_t cpu_read(uintptr_t addr, int len);
 void cpu_write(uintptr_t addr, int len, uint32_t data);
